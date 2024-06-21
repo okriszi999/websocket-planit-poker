802823a14eedd8df2890c1e81f7872dd5fb38926
Krisztian Olah <krisztian.olah@genesys.com>
2024-06-21 14:27:47 +0200

    chore: Add initial project files and configurations

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
+</script>
+
+<template>
+  <div>
+    Socket io stuff
+    <Button>
+      Hello
+    </Button>
+  </div>
+</template>
---
+++ server/api/handler.ts
@@ -0,0 +1,5 @@
+export default defineEventHandler(async (event) => {
+  return {
+    text: "Hello, world!",
+  };
+});
