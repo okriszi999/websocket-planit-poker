fc16349cb0aa4536dbf4021468593a885221b2f6
Krisztian Olah <krisztian.olah@genesys.com>
2024-06-21 14:45:07 +0200

    add websocket solution

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"]
-})
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  nitro: {
+    experimental: {
+      websocket: true,
+    },
+  },
+});
